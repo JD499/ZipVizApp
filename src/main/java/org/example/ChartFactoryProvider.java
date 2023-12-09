a1f1fb02049158c5f82a720547af0aea7e70fed0
James <jd499b@gmail.com>
2023-12-08 19:01:16 -0800

    Add new chart factories and methods in ChartFactoryProvider
    Expanded the ChartFactoryProvider class to include additional chart factories, such as the InitialChartFactory. New methods were also implemented to return individual instances of each chart factory type, enhancing modularity and ease of use.

--- src/main/java/org/example/ChartFactoryProvider.java
+++ src/main/java/org/example/ChartFactoryProvider.java
@@ -7,7 +7,29 @@ public class ChartFactoryProvider {
             case "Lorenz Curve" -> new LorenzChartFactory();
             case "Pie Chart" -> new PieChartFactory();
             case "Line Chart" -> new LineChartFactory();
+            case "Initial Chart" -> new InitialChartFactory();
             default -> throw new IllegalArgumentException("Invalid chart type: " + chartType);
         };
     }
+
+
+    public ChartFactory getBarChartFactory() {
+        return new BarChartFactory();
+    }
+
+    public ChartFactory getLorenzChartFactory() {
+        return new LorenzChartFactory();
+    }
+
+    public ChartFactory getPieChartFactory() {
+        return new PieChartFactory();
+    }
+
+    public ChartFactory getLineChartFactory() {
+        return new LineChartFactory();
+    }
+    public ChartFactory getInitialChartFactory() {
+        return new InitialChartFactory();
+    }
+
 }
